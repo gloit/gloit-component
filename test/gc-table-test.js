91e671c0dbf24169db8bbac9b30e8e43cfd64c06
Tower He <towerhe@gmail.com>
2014-09-08 17:59:34 +0800

    Fix path of cellView.

--- test/gc-table-test.js
+++ test/gc-table-test.js
@@ -1,5 +1,31 @@
 moduleForComponent('gc-table');
 
+var setupTable = function(table) {
+  table.set('columns', [
+    GloitComponent.TableColumnModel.create({
+      title: '名称',
+      cellContentPath: 'name'
+    }),
+    GloitComponent.TableColumnModel.create({
+      title: '性别',
+      cellContentPath: 'gender'
+    })
+  ]);
+
+  table.set('content', [
+    Ember.Object.create({
+      name: '张三',
+      gender: '男'
+    }),
+    Ember.Object.create({
+      name: '李四',
+      gender: '女'
+    })
+  ]);
+
+  return table;
+};
+
 test('renders rows', function() {
   expect(2);
 
@@ -8,27 +34,7 @@ test('renders rows', function() {
   ok(!Ember.isEmpty(this.$('.gc-table')), 'exists');
 
   Ember.run(function() {
-    table.set('columns', [
-      GloitComponent.TableColumnModel.create({
-        title: '名称',
-        cellContentPath: 'name'
-      }),
-      GloitComponent.TableColumnModel.create({
-        title: '性别',
-        cellContentPath: 'gender'
-      })
-    ]);
-
-    table.set('content', [
-      Ember.Object.create({
-        name: '张三',
-        gender: '男'
-      }),
-      Ember.Object.create({
-        name: '李四',
-        gender: '女'
-      })
-    ]);
+    setupTable(table);
   });
 
   equal(table.$('tbody tr').length, 2, 'has two records in rows');
@@ -40,10 +46,27 @@ test('renders top actions', function() {
   var table = this.subject();
 
   Ember.run(function() {
+    setupTable(table);
+
     table.set('topActions', [{
       name: 'removeSelected', text: '移除选定', icon: 'fa fa-minus', position: 'left'
     }]);
   });
 
   equal(this.$('caption .gc-table-action-group.pull-left a').text().trim(), '移除选定', 'renders left side actions');
+});
+
+test('enables multi-selectable', function() {
+  expect(1);
+
+  var table = this.subject();
+
+  Ember.run(function() {
+    setupTable(table);
+
+    table.set('rowSelectable', true);
+    table.set('multiple', true);
+  });
+
+  ok(!Ember.isEmpty(this.$('thead input[type=checkbox]')), 'has a checkbox in head');
 });
