04e375ccb58da6db06485889a20d7691bd389e40
Tower He <towerhe@gmail.com>
2014-12-02 02:20:32 +0800

    Can provide a context to link-to

--- dist/named-amd/gloit-component.js
+++ dist/named-amd/gloit-component.js
@@ -1448,23 +1448,42 @@ define("gloit-component/templates/gc-sidelist/gc-item",
     __exports__["default"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
     this.compilerInfo = [4,'>= 1.0.0'];
     helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
-      var buffer = '', stack1, helper, options, escapeExpression=this.escapeExpression, self=this, helperMissing=helpers.helperMissing;
+      var buffer = '', stack1, escapeExpression=this.escapeExpression, self=this, helperMissing=helpers.helperMissing;
 
     function program1(depth0,data) {
       
+      var buffer = '', stack1, helper, options;
+      data.buffer.push("\n  ");
+      stack1 = (helper = helpers['link-to'] || (depth0 && depth0['link-to']),options={hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(2, program2, data),contexts:[depth0,depth0],types:["ID","ID"],data:data},helper ? helper.call(depth0, "view.content.route", "view.content.context", options) : helperMissing.call(depth0, "link-to", "view.content.route", "view.content.context", options));
+      if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+      data.buffer.push("\n");
+      return buffer;
+      }
+    function program2(depth0,data) {
+      
       var buffer = '', stack1;
-      data.buffer.push("\n  <i ");
+      data.buffer.push("\n    <i ");
       data.buffer.push(escapeExpression(helpers['bind-attr'].call(depth0, {hash:{
         'class': ("view.content.icon")
       },hashTypes:{'class': "ID"},hashContexts:{'class': depth0},contexts:[],types:[],data:data})));
-      data.buffer.push("></i>\n  ");
+      data.buffer.push("></i>\n    ");
       stack1 = helpers._triageMustache.call(depth0, "view.content.text", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+      if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+      data.buffer.push("\n  ");
+      return buffer;
+      }
+
+    function program4(depth0,data) {
+      
+      var buffer = '', stack1, helper, options;
+      data.buffer.push("\n  ");
+      stack1 = (helper = helpers['link-to'] || (depth0 && depth0['link-to']),options={hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(2, program2, data),contexts:[depth0],types:["ID"],data:data},helper ? helper.call(depth0, "view.content.route", options) : helperMissing.call(depth0, "link-to", "view.content.route", options));
       if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
       data.buffer.push("\n");
       return buffer;
       }
 
-      stack1 = (helper = helpers['link-to'] || (depth0 && depth0['link-to']),options={hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],data:data},helper ? helper.call(depth0, "view.content.route", options) : helperMissing.call(depth0, "link-to", "view.content.route", options));
+      stack1 = helpers['if'].call(depth0, "view.content.context", {hash:{},hashTypes:{},hashContexts:{},inverse:self.program(4, program4, data),fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],data:data});
       if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
       data.buffer.push("\n");
       return buffer;
