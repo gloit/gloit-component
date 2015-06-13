ce712572c5b30d298af556a8c90524d2441bfb41
towerhe <towerhe@gmail.com>
2015-06-13 23:26:50 +0800

    Add missing files

---
+++ README.md
@@ -0,0 +1,44 @@
+# Gloit Component
+
+This README outlines the details of collaborating on this Ember addon.
+
+## Dependencies
+
+### NPM
+
+* ember-cli-less
+* ember-cli-i18n
+* ember-crumbly
+
+### Bower
+
+* animate.css
+* pace
+* slimscroll
+* bootstrap
+* fontawesome
+* numeral
+* toastr
+* googlefonts (https://github.com/towerhe/googlefonts.git)
+
+## Installation
+
+* `git clone` this repository
+* `npm install`
+* `bower install`
+
+## Running
+
+* `ember server`
+* Visit your app at http://localhost:4200.
+
+## Running Tests
+
+* `ember test`
+* `ember test --server`
+
+## Building
+
+* `ember build`
+
+For more information on using ember-cli, visit [http://www.ember-cli.com/](http://www.ember-cli.com/).
---
+++ index.js
@@ -0,0 +1,70 @@
+/* jshint node: true */
+'use strict';
+
+var mergeTrees = require( 'broccoli-merge-trees' ),
+    pickFiles  = require( 'broccoli-static-compiler' );
+
+module.exports = {
+  name: 'gloit-component',
+
+  included: function (app) {
+    app.import({
+      development: 'bower_components/toastr/toastr.css',
+      production: 'bower_components/toastr/toastr.min.css'
+    });
+
+    app.import({
+      development: 'bower_components/bootstrap/dist/js/bootstrap.js',
+      production: 'bower_components/bootstrap/dist/js/bootstrap.min.js'
+    });
+
+    app.import({
+      development: 'bower_components/animate.css/animate.css',
+      production: 'bower_components/animate.css/animate.min.css'
+    });
+
+    app.import({
+      development: 'bower_components/slimscroll/jquery.slimscroll.js',
+      production: 'bower_components/slimscroll/jquery.slimscroll.min.js'
+    });
+
+    app.import({
+      development: 'bower_components/pace/pace.js',
+      production: 'bower_components/pace/pace.min.js'
+    });
+
+    app.import({
+      development: 'bower_components/numeral/numeral.js',
+      production: 'bower_components/numeral/min/numeral.min.js'
+    });
+
+    app.import({
+      development: 'bower_components/toastr/toastr.js',
+      production: 'bower_components/toastr/toastr.min.js'
+    });
+
+    app.import('bower_components/sweetalert/dist/sweetalert.css');
+
+    app.import({
+      development: 'bower_components/sweetalert/dist/sweetalert-dev.js',
+      production: 'bower_components/sweetalert/dist/sweetalert.min.js',
+    });
+  },
+
+  postprocessTree: function(type, tree) {
+    return mergeTrees([tree,
+                      pickFiles('bower_components/fontawesome/fonts', {
+                        srcDir: '/',
+                        files: ['*.*'],
+                        destDir: '/fonts'
+                      }),
+                      pickFiles('bower_components/googlefonts/fonts', {
+                        srcDir: '/',
+                        files: ['*.woff2'],
+                        destDir: '/assets/fonts'
+                      })
+    ], {
+      overwrite: true
+    });
+  }
+};
